16f68397c31a619337b15b2af8d358dea746997d
Yuri <yuri.araujo.silva@gmail.com>
2021-10-20 21:50:48 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-import { Slide, SlideNav } from "./slide.js";
+import  SlideNav from "./slide.js";
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
+slide.addControl('.custom-controls');
 
-slide.addControl();
 
-console.log(slide);
